6df6c9a1c3477628a4ee1519ee21fbe8a9173a6b
David McSharry <dm2223@gpu34.doc.ic.ac.uk>
2024-04-03 02:59:23 +0100

    first proper run

---
+++ kraskov_mi.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+import numpy as np
+from scipy.spatial.distance import squareform, pdist
+
+def pyMIestimator(X,Y,k=5,base=np.exp(1)):
+#    pyMIestimator is a function for estimating Mutual Information
+#    Copyright (C) 2017 Aalto University (Surfaces and Interfaces at 
+#    the Nanoscale (SIN)), Finland
+#
+#    This program is free software: you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public License
+#    as published by the Free Software Foundation, either version 3 of
+#    the License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this program.  If not, see
+#    <http://www.gnu.org/licenses/>.
+#
+#    Martha Arbayani Zaidan, PhD
+#    Postdoctoral Research Fellow
+#    Aalto University, Finland
+#
+#    Yashasvi Singh Ranawat 
+#    yashasvi.ranawat@gmail.com
+#    Aalto University, Finland
+#
+# Inputs function:
+# X,Y         : an N x ... tensors (samples N features)
+# k           : the number of nearest neighbour
+# unit base   : base=2 -> Shannon (bits), base=exp(1)=nat
+#
+# Outputs function:
+# I1, I2      : mutual information estimates
+#
+# References:
+#
+#
+#    Jorge Numata, Oliver Ebenhoh and Ernst-Walter Knapp,
+#    Measuring correlations in metabolomic networks with mutual information,
+#    Genome Informatics 20 (2008): 112-122.
+
+    N = X.shape[0] #  The number of samples
+    
+    # distance matrices
+    dist_X = squareform(pdist(X.reshape((N, -1))))
+    dist_Y = squareform(pdist(Y.reshape((N, -1))))
+
+    # scaling
+    dist_X /= np.std(dist_X, axis=1, keepdims=True)
+    dist_Y /= np.std(dist_Y, axis=1, keepdims=True)
+
+    # index matrix
+    dist_Z = np.maximum(dist_X, dist_Y)
+    indx = np.argsort(dist_Z)[:, k-1]
+
+    # psi
+    psi = 0
+
+    for i in range(N):
+        nx = np.sum(dist_X[i, :] <= dist_X[i, indx[i]])
+        ny = np.sum(dist_Y[i, :] <= dist_Y[i, indx[i]])
+        psi += digamma(nx) + digamma(ny)
+
+    # mutual information
+    I = (digamma(k) - 1/float(k)
+          - (psi / N)
+          + digamma(N) ) / np.log(base)
+
+    # No negative MI:
+    if I < 0: I = 0
+
+    return I
+
+'''
+#digamma function using recursion; 
+#not used since maximum recursion depth exceeds in comparison
+def digamma(x):
+    if x == 1:
+        #Euler-Mascheroni constant
+        return -0.57721566490153286060651209008240243104215933593992 
+    else:
+        return 1/float(x-1) + digamma(x-1)
+'''
+
+#digamma function using while; 
+def digamma(x):
+    count = 1
+    #Euler-Mascheroni constant
+    value = -0.57721566490153286060651209008240243104215933593992 
+    while count < int(x):
+        value += 1/float(count)
+        count += 1
+    return value
+    
+##Execution
+
+if __name__ == "__main__":
+    
+    import sys
+    import os
+    
+    if len(sys.argv) < 3:
+        print("\n//Usage: ", sys.argv[0],
+               " <numpy array file 1> <numpy array file 2>" \
+               " <optional: index for .dat file>\n")
+        sys.exit()
+        
+    arg = sys.argv[1:]
+    k = 5
+    for i in range(len(arg)):
+        if arg[i].startswith('-k'):
+            k = int(arg[i][2:])
+            arg.pop(i)
+            break
+
+    for i in arg[:1]:
+        if not os.path.isfile(i):
+            print("\n//Error: ", i, " not found\n")
+            sys.exit()
+            
+    X = np.load(arg[0])
+    Y = np.load(arg[1])
+    
+    if not X.shape[0] == Y.shape[0]:
+        print('Array index 1 size mismatch')
+        sys.exit()
+    
+    I = pyMIestimator(X,Y,k)
+    with open('Mutual_information-k' + str(k) + '.dat', 'a') as f:
+        f.write('I('+ arg[0] + ';' + arg[1] + ') = ' + str(I)
+                + (' ' + arg[2] if len(arg) > 2 else '') + '\n')
+    
---
+++ utils.py
@@ -0,0 +1,127 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import torch
+import scipy.io
+from torch.utils.data import TensorDataset, DataLoader
+from scipy.signal import butter, filtfilt, decimate
+from scipy.fftpack import fft, fftfreq
+from sklearn.decomposition import PCA
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def estimate_MI_smile(scores):
+    """
+    Returns the MI estimate using the SMILE estimator given the scores matrix and a clip
+    """
+    clip = 5
+    
+    first_term = scores.diag().mean()
+
+    batch_size = scores.size(0)
+
+    # clip scores between -clip and clip
+    clipped_scores = torch.clamp(scores, -clip, clip)
+    clipped_scores = scores
+
+    # e^clipped_scores
+    exp_clipped_scores = torch.exp(clipped_scores)
+
+    mask = (torch.ones_like(exp_clipped_scores).to(device) - torch.eye(batch_size).to(device))
+
+    masked_exp_clipped_scores = exp_clipped_scores * mask
+
+    num_non_diag = mask.sum()
+
+    mean_exp_clipped_scores = masked_exp_clipped_scores.sum() / num_non_diag
+
+    second_term = torch.log2(mean_exp_clipped_scores)
+
+    return (1/torch.log(torch.tensor(2.0))) * first_term - second_term
+
+
+# def fft_check(data, fs):
+#     # Perform FFT on your data and plot results to view frequencies present in data
+#     N = data.shape[0]
+#     yf = fft(data)
+#     xf = fftfreq(N, 1/fs)
+
+#     plt.figure(figsize=(10,6))
+#     plt.plot(xf, np.abs(yf))
+
+#     plt.title('FFT of data')
+#     plt.xlabel('Frequency (Hz)')
+#     plt.ylabel('Magnitude')
+#     plt.grid()
+#     plt.xscale('log')
+#     plt.show()
+
+#     return None
+
+
+# https://neurotycho.brain.riken.jp/download/2012/20100802S1_Epidural-ECoG+Food-Tracking_B_Kentaro+Shimoda_mat_ECoG64-Motion6.zip
+
+def prepare_ecog_dataset():
+
+
+    def _butter_highpass(cutoff, fs, order=5):
+        nyq = 0.5 * fs
+        normal_cutoff = cutoff / nyq
+        b, a = butter(order, normal_cutoff, btype='high', analog=False)
+        return b, a
+
+
+    def _butter_highpass_filter(data, cutoff, fs, order=5):
+        b, a = _butter_highpass(cutoff, fs, order=order)
+        y = filtfilt(b, a, data)
+        return y
+
+    
+    num_channels = 64
+    data_list = []
+    for i in range(1, num_channels + 1):
+
+        channel_data = scipy.io.loadmat(f'/vol/bitbucket/dm2223/info-theory-experiments/data/ecog_data_raw/ECoG_ch{i}.mat')
+        data = channel_data[f'ECoGData_ch{i}'].squeeze()
+
+        # high pass filter at 1 Hz
+        fs = 1000  # original sampling rate
+        cutoff = 1  # cutoff frequency for high pass filter
+        data = _butter_highpass_filter(data, cutoff, fs)
+
+        # downsample to 300 Hz
+        downsample_rate = int(fs / 300)  # calculate downsample rate
+        data = decimate(data, downsample_rate)
+
+        # standardise across features. Comes last as the data being fed into our ML model should be standardised
+        data = (data - np.mean(data)) / np.std(data)
+
+        data_list.append(data) 
+
+    # Stack all channel data into a single numpy array
+    all_data = np.stack(data_list, axis=0)
+
+    dataset_tensor = torch.tensor(all_data).T
+
+    # save as dataset
+    torch.save(dataset_tensor, '/vol/bitbucket/dm2223/info-theory-experiments/data/ecog_data')
+
+    return None
+
+
+
+
+def prepare_batch(X):
+    # take all samples except the last one as inputs
+    input_data = X[:-1]
+
+    # take all samples except the first one as targets
+    target_data = X[1:]
+
+    # stack them as pairs with dimension (999, 2, 10)
+    pairs = torch.stack((input_data, target_data), dim=1)
+    
+    assert pairs[0,0,0] == input_data[0,0]
+
+    return pairs
