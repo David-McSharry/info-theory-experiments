8888b3b1622e61782a0025e959e0d2f419315b28
David-McSharry <dmcsharr@tcd.ie>
2024-05-05 21:11:15 +0100

    skip connection netwokr

--- models.py
+++ models.py
@@ -1,9 +1,8 @@
 import torch
 import torch.nn as nn
 from smile_estimator import estimate_mutual_information
-# spectral norm
 from torch.nn.utils.spectral_norm import spectral_norm
-# NOTE: spectral norm removed for now
+import torch.nn.functional as F
 
 class SupervenientFeatureNetwork(nn.Module):
     def __init__(
@@ -25,6 +24,44 @@ class SupervenientFeatureNetwork(nn.Module):
 
     def forward(self, x):
         return self.f(x)
+    
+
+
+class SkipConnectionSupervenientFeatureNetwork(nn.Module):
+    def __init__(self, num_atoms: int, feature_size: int, hidden_sizes: list, include_bias: bool = True):
+        super(SkipConnectionSupervenientFeatureNetwork, self).__init__()
+
+        if not hidden_sizes:
+            raise ValueError("hidden_sizes must contain at least one element")
+
+        # Use the first hidden layer size for the initial projection
+        self.initial_projection = nn.Linear(num_atoms, hidden_sizes[0], bias=include_bias)
+
+        # Creating the hidden layers with skip connections
+        self.hidden_layers = nn.ModuleList()
+        input_size = hidden_sizes[0]
+        for hidden_size in hidden_sizes:
+            layer = nn.Linear(input_size, hidden_size, bias=include_bias)
+            self.hidden_layers.append(layer)
+            input_size = hidden_size
+
+        # Final downprojection to the feature size
+        self.final_projection = nn.Linear(input_size, feature_size, bias=include_bias)
+
+    def forward(self, x):
+        # Initial projection
+        x = self.initial_projection(x)
+
+        # Passing through each hidden layer with ReLU and skip connection
+        for layer in self.hidden_layers:
+            identity = x
+            x = layer(x)
+            x = F.relu(x)
+            x = torch.add(x, identity)
+
+        # Final projection without non-linearity
+        x = self.final_projection(x)
+        return x
 
 
 class DecoupledSmileMIEstimator(nn.Module):
