c8b9576383fbf2fd7427bf4c48d42b158a2cc6e4
David McSharry <dmcsharr@tcd.ie>
2024-08-03 01:50:05 +0000

    to use RNN files update trainers

---
+++ diversity_abl_exp.py
@@ -0,0 +1,133 @@
+
+from custom_datasets import BitStringDataset
+from models import SupervenientFeatureNetwork
+import torch
+from models import SkipConnectionSupervenientFeatureNetwork
+from trainers import train_feature_network
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+for seed in range(6,10):
+    bits_config_train = {
+            "gamma_parity": 0.99,
+            "gamma_extra": 0.99,
+            "dataset_length": 1000000,
+            "torch_seed": seed,
+            "dataset_type": "bits",
+            "num_atoms": 6,
+            "batch_size": 1000,
+            "train_mode": True,
+            "train_model_B": False,
+            "adjust_Psi": False,
+            "clip": 5,
+            "feature_size": 1,
+            "epochs": 5,
+            "start_updating_f_after": 1000,
+            "update_f_every_N_steps": 5,
+            "minimize_neg_terms_until": 0,
+            "downward_critics_config": {
+                "hidden_sizes_v_critic": [512, 512, 512, 256],
+                "hidden_sizes_xi_critic": [512, 512, 512, 256],
+                "critic_output_size": 32,
+                "lr": 1e-3,
+                "bias": True,
+                "weight_decay": 0,
+            },
+            
+            "decoupled_critic_config": {
+                "hidden_sizes_encoder_1": [512, 512, 512],
+                "hidden_sizes_encoder_2": [512, 512, 512],
+                "critic_output_size": 32,
+                "lr": 1e-3,
+                "bias": True,
+                "weight_decay": 0,
+            },
+            "feature_network_config": {
+                "hidden_sizes": [256, 256],
+                "lr": 1e-4,
+                "bias": True,
+                "weight_decay": 0.00001,
+            }
+    }
+
+    dataset = BitStringDataset(
+        gamma_extra=bits_config_train["gamma_extra"],
+        gamma_parity=bits_config_train["gamma_parity"],
+        length=bits_config_train["dataset_length"],
+    )
+
+    trainloader = torch.utils.data.DataLoader(
+        dataset, batch_size=bits_config_train["batch_size"], shuffle=False
+    )
+
+    skip_model = SkipConnectionSupervenientFeatureNetwork(
+        num_atoms=bits_config_train['num_atoms'],
+        feature_size=bits_config_train['feature_size'],
+        hidden_sizes=bits_config_train['feature_network_config']['hidden_sizes'],
+        include_bias=bits_config_train['feature_network_config']['bias'],
+    ).to(device)
+
+    project_name_train = "NEURIPS-diversity-ablation-training"
+
+    skip_model = train_feature_network(
+            config=bits_config_train,
+            trainloader=trainloader,
+            feature_network_training=skip_model,
+            project_name=project_name_train,
+            model_dir_prefix=None
+    )
+
+    bits_config_test = {
+            "gamma_parity": 0.99,
+            "gamma_extra": 0.99,
+            "dataset_length": 1000000,
+            "torch_seed": seed,
+            "dataset_type": "bits",
+            "num_atoms": 6,
+            "batch_size": 1000,
+            "train_mode": False,
+            "train_model_B": False,
+            "adjust_Psi": False,
+            "clip": 5,
+            "feature_size": 1,
+            "epochs": 2,
+            "start_updating_f_after": 1000,
+            "update_f_every_N_steps": 5,
+            "minimize_neg_terms_until": 0,
+            "downward_critics_config": {
+                "hidden_sizes_v_critic": [256, 256, 256],
+                "hidden_sizes_xi_critic": [256, 256, 256],
+                "critic_output_size": 32,
+                "lr": 1e-3,
+                "bias": True,
+                "weight_decay": 0,
+            },
+            
+            "decoupled_critic_config": {
+                "hidden_sizes_encoder_1": [256, 256],
+                "hidden_sizes_encoder_2": [256, 256],
+                "critic_output_size": 32,
+                "lr": 1e-3,
+                "bias": True,
+                "weight_decay": 0,
+            },
+            "feature_network_config": {
+                "hidden_sizes": [256, 256],
+                "lr": 1e-4,
+                "bias": True,
+                "weight_decay": 0.00001,
+            }
+    }
+
+    project_name_test = "NEURIPS-diversity-ablation-test"
+
+    skil_model = train_feature_network(
+            config=bits_config_test,
+            trainloader=trainloader,
+            feature_network_training=skip_model,
+            project_name=project_name_test,
+            model_dir_prefix=None
+    )
+
+
+
